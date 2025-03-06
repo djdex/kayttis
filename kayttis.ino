c602e8c6a150d239ce9e0bdd3bc3f130245a39ff
unknown <dexton.vige@aalto.fi>
2025-03-06 14:49:20 +0200

    Basic functionality complete

--- kayttis.ino
+++ kayttis.ino
@@ -1,6 +1,6 @@
 #include <ezButton.h>
 
-const int buzPin = 7;
+const int buzzPin = 7;
 ezButton keySwitch(8);
 const int trigPin = 9;
 const int echoPin = 10;
@@ -9,12 +9,31 @@ float timing = 0.0;
 float distance = 0.0;
 int keyState = 0;
 
+const int doorDistance = 20;
+bool doorOpened = false;
+//Time
+const int keyWait = 5000; //Oikeassa 5min
+int prevTime = 0;
+int proxTimer = 0;
+int deltaTime = 0;
+int keysTakenTime = 0;
+
+void decrementTimer() {
+  proxTimer = max(proxTimer - deltaTime, 0);
+
+  if (keyState == LOW) {
+    keysTakenTime = 0;
+  } else {
+    keysTakenTime += deltaTime;
+  }
+}
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(echoPin, INPUT);
   pinMode(trigPin, OUTPUT);
   pinMode(ledPin, OUTPUT);
-  pinMode(buzPin, OUTPUT);
+  pinMode(buzzPin, OUTPUT);
 
   keySwitch.setDebounceTime(50);
   digitalWrite(trigPin, LOW);
@@ -23,31 +42,55 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  deltaTime = millis() - prevTime;
+  prevTime = millis();
+  decrementTimer();
+
+  
   keySwitch.loop();
 
-  digitalWrite(trigPin, LOW);
-  delay(2);
-  
-  digitalWrite(trigPin, HIGH);
-  delay(10);
-  digitalWrite(trigPin, LOW);
-  timing = pulseIn(echoPin, HIGH);
-  distance = (timing * 0.034) / 2;
-  Serial.print("Distance: ");
-  Serial.print(distance);
-  
-  keyState = keySwitch.getState();
-  if (distance < 15 && keyState == LOW) {
-    digitalWrite(ledPin, HIGH);
-    digitalWrite(buzPin, HIGH);
-  }
-  else{
-    digitalWrite(ledPin, LOW);
-    digitalWrite(buzPin, LOW);
+  if (proxTimer == 0) {
+    digitalWrite(trigPin, LOW);
+
+    delay(2);
+    
+    digitalWrite(trigPin, HIGH);
+
+    delay(10);
+
+    digitalWrite(trigPin, LOW);
+    timing = pulseIn(echoPin, HIGH);
+    distance = (timing * 0.034) / 2;
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println("cm");
+    Serial.print(keysTakenTime);
+    Serial.println("ms since keys last taken");
+    proxTimer = 100;
   }
 
-  Serial.print("cm | ");
-  Serial.print(distance / 2.54);
-  Serial.println("in");
-  delay(100);
+  keyState = keySwitch.getState();
+
+  
+  if (distance > doorDistance) {
+    doorOpened = true;
+    if (keyState == LOW) {
+      digitalWrite(ledPin, HIGH);
+      digitalWrite(buzzPin, HIGH);
+    } else {
+      if (keysTakenTime > keyWait) {
+        digitalWrite(ledPin, HIGH);
+        digitalWrite(buzzPin, LOW);
+      } 
+    }  
+  } else {
+    doorOpened = false;
+    
+    digitalWrite(buzzPin, LOW);
+
+    if (keyState == LOW) {
+      digitalWrite(ledPin, LOW);
+    }
+  }
 }
