d96feb665558deeb4742dc050d18c3c4266c816e
unknown <dexton.vige@aalto.fi>
2025-03-07 14:11:57 +0200

    Sleep nappi

--- kayttis.ino
+++ kayttis.ino
@@ -5,6 +5,7 @@ ezButton keySwitch(8);
 const int trigPin = 9;
 const int echoPin = 10;
 const int ledPin = 11;
+ezButton sleepBut(12);
 float timing = 0.0;
 float distance = 0.0;
 int keyState = 0;
@@ -15,11 +16,14 @@ bool doorOpened = false;
 const int keyWait = 5000; //Oikeassa 5min
 int prevTime = 0;
 int proxTimer = 0;
+int sleepTimer = 0;
+const int sleepTime = 5000;
 int deltaTime = 0;
 int keysTakenTime = 0;
 
 void decrementTimer() {
   proxTimer = max(proxTimer - deltaTime, 0);
+  sleepTimer = max(sleepTimer - deltaTime, 0);
 
   if (keyState == LOW) {
     keysTakenTime = 0;
@@ -36,6 +40,7 @@ void setup() {
   pinMode(buzzPin, OUTPUT);
 
   keySwitch.setDebounceTime(50);
+  sleepBut.setDebounceTime(50);
   digitalWrite(trigPin, LOW);
   Serial.begin(9600);
 }
@@ -49,6 +54,7 @@ void loop() {
 
   
   keySwitch.loop();
+  sleepBut.loop();
 
   if (proxTimer == 0) {
     digitalWrite(trigPin, LOW);
@@ -72,14 +78,20 @@ void loop() {
 
   keyState = keySwitch.getState();
 
+  Serial.println(sleepBut.getState());
+  if (sleepBut.getState() == HIGH) {
+    sleepTimer = sleepTime;
+  }
+
   
-  if (distance > doorDistance) {
+  if (distance > doorDistance && sleepTimer == 0) {
     doorOpened = true;
     if (keyState == LOW) {
       digitalWrite(ledPin, HIGH);
       digitalWrite(buzzPin, HIGH);
     } else {
       if (keysTakenTime > keyWait) {
+        //Tässä voisi laittaa hälyn pois päältä, kunnes ovi suljetaan taas seuraavan kerran
         digitalWrite(ledPin, HIGH);
         digitalWrite(buzzPin, LOW);
       } 
@@ -89,7 +101,7 @@ void loop() {
     
     digitalWrite(buzzPin, LOW);
 
-    if (keyState == LOW) {
+    if (keyState == LOW || sleepTimer != 0) {
       digitalWrite(ledPin, LOW);
     }
   }
